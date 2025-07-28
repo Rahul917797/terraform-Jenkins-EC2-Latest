876b1c26fbda3a33575ae1d7bae9054e91fc6f1c
Rahul917797 <101493883+Rahul917797@users.noreply.github.com>
2025-07-28 16:20:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "myprivatebucket-05011997-28072025"
+    key = "terraform.tfstate"
+    region = us-east-1
+   }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
